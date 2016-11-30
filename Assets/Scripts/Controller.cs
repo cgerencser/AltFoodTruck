98f81e051f2affbc4e2e8f9704e401268e359563
Christopher Gerencser <cgerencser@carpenter-11.elon.edu>
2016-11-29 19:14:01 -0500

    almost done Canvas UI

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,54 +10,116 @@ public class Controller : MonoBehaviour
     private GameObject instructions;
     [SerializeField]
     private GameObject quit;
-    private bool isOn;
+	[SerializeField]
+	private GameObject pause;
+	[SerializeField]
+	private GameObject controls;
+	[SerializeField]
+	private GameObject end;
+    private bool timeOn;
     private bool exit;
     // Use this for initialization
     void Start()
     {
-        isOn = false;
+		menu.SetActive (true);
+		instructions.SetActive (true);
+		quit.SetActive (false);
+		controls.SetActive (false);
+		end.SetActive (false);
+		pause.SetActive (false);
+        timeOn = true;
         exit = false;
     }
 
+	public void ControlMenu(){
+		pause.SetActive (false);
+		controls.SetActive (true);
+	}
+
+	public void QuitMenu(){
+		pause.SetActive (false);
+		quit.SetActive (true);
+	}
+
+	public void Continue(){
+		timeOn = !timeOn;
+		Time.timeScale = 1;
+		menu.SetActive (true);
+		pause.SetActive (false);
+		Cursor.visible = false;
+	}
+
+	public void QuitNo(){
+		quit.SetActive (false);
+		pause.SetActive (true);
+	}
+		
+	public void QuitYes(){
+
+	}
+
+	public void ControlsBack(){
+		controls.SetActive (false);
+		pause.SetActive (true);
+	}
     // Update is called once per frame
     void Update()
     {
-        if (!exit)
-        {
-            if (Input.GetKeyDown("p"))
-            {
-                isOn = !isOn;
-                instructions.SetActive(isOn);
-                menu.SetActive(!isOn);
-            }
+		if (timeOn) {
+			if (Input.GetKeyDown ("p")) {
+				timeOn = !timeOn;
+				Time.timeScale = 0;
+				menu.SetActive (false);
+				pause.SetActive (true);
+				instructions.SetActive (false);
+				Cursor.visible = true;
+			}
+		} else {
+			if (Input.GetKeyDown ("p")) {
+				timeOn = !timeOn;
+				Time.timeScale = 1;
+				menu.SetActive (true);
+				pause.SetActive (false);
+				Cursor.visible = false;
+			}
+		}
 
-            if (Input.GetKeyDown("q"))
-            {
-                quit.SetActive(true);
-                instructions.SetActive(false);
-                menu.SetActive(false);
-                Time.timeScale = 0;
-                exit = true;
-            }
-        }
-        else
-        {
-            if (Input.GetKeyDown("y"))
-            {
-                SceneManager.LoadScene("Truck");
-                Time.timeScale = 1;
-            }
-            if (Input.GetKeyDown("n"))
-            {
-                Time.timeScale = 1;
-                exit = false;
-                quit.SetActive(false);
-                instructions.SetActive(isOn);
-                menu.SetActive(!isOn);
-
-            }
-        }
+//		if (!exit)
+//        {
+//            if (Input.GetKeyDown("p"))
+//            {
+//                isOn = !isOn;
+//                instructions.SetActive(isOn);
+//                menu.SetActive(!isOn);
+//            }
+//
+//            if (Input.GetKeyDown("q"))
+//            {
+//                quit.SetActive(true);
+//                instructions.SetActive(false);
+//                menu.SetActive(false);
+//                Time.timeScale = 0;
+//                exit = true;
+//            }
+//        }
+//        else
+//        {
+//            if (Input.GetKeyDown("y"))
+//            {
+//                SceneManager.LoadScene("Truck");
+//                Time.timeScale = 1;
+//            }
+//            if (Input.GetKeyDown("n"))
+//            {
+//                Time.timeScale = 1;
+//                exit = false;
+//                quit.SetActive(false);
+//                instructions.SetActive(isOn);
+//                menu.SetActive(!isOn);
+//
+//            }
+//        }
     }
 }
-    
+
 
